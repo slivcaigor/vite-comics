ed0d462ff150c3a9ced9128a89c53c24fbc40b6b
slivcaigor <slivcaigor@gmail.com>
2022-12-12 14:12:50 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script>
+import AppHeader from './components/AppHeader.vue'
+import AppFooter from './components/AppFooter.vue'
+import AppFeatures from './components/AppFeatures.vue'
+
+export default {
+  components: {
+    AppHeader,
+    AppFooter,
+    AppFeatures
+  }
+}
+
+</script>
+
+<template>
+
+  <AppHeader />
+  <AppFeatures />
+  <AppFooter />
+
+</template>
+
+<style lang="scss" scoped>
+@use './styles/general.scss' as *;
+@use './styles/partials/variables' as *;
+</style>
---
+++ src/components/AppFooter.vue
@@ -0,0 +1,19 @@
+<script>
+import AppFeatures from './components/AppFeatures.vue'
+
+export default {
+  components: {
+    AppFeatures
+  }
+}
+
+</script>
+
+<template>
+  <AppFeatures />
+</template>
+
+
+<style lang="scss" scoped>
+
+</style>
