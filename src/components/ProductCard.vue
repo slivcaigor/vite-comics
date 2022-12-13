e4edb1022ef963e9cefe2cd2c31be224e5bb8f69
slivcaigor <slivcaigor@gmail.com>
2022-12-13 16:23:33 +0100

    Added pricing and hover on card

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -11,7 +11,9 @@ export default {
 
   <div class="cards">
     <img :src="details.thumb" :alt="details.series">
+    <h5>{{ details.price }}</h5>
     <h3>{{ details.series }}</h3>
+    <span class="label">{{ details.type }}</span>
   </div>
 
 
@@ -24,12 +26,37 @@ export default {
 @use '../styles/partials/variables' as *;
 
 .cards {
+  position: relative;
+
+  &:hover .label {
+    visibility: visible;
+  }
+
+  .label {
+    visibility: hidden;
+    top: 5%;
+    right: 0;
+    position: absolute;
+    background-color: $accent;
+    padding: .2em .5em;
+    color: $primary;
+    font-size: 1rem;
+    font-weight: 600;
+    text-transform: uppercase;
+  }
+
+  h5 {
+    color: $accent;
+    text-transform: uppercase;
+    padding-top: .2em;
+    font-size: 1rem;
+  }
 
   h3 {
     color: $primary;
     text-transform: uppercase;
-    padding-top: .7em;
     font-size: 1rem;
+    padding-top: .2em;
   }
 }
 </style>
